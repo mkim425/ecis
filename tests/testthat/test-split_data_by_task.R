74ce27dbf7e370ee4aa4c05c545d1284fe17c3a5
mkim425 <minsu@umass.edu>
2024-01-18 14:24:22 -0500

    added a check for unique task_id

--- tests/testthat/test-split_data_by_task.R
+++ tests/testthat/test-split_data_by_task.R
@@ -9,4 +9,18 @@ test_that("split_data_by_task() groups data correctly", {
         # Check if the result is a list
         expect_type(result, "list")
 
+        # Check if each data set has unique task_id
+        expect_true(all(
+                sapply(
+                        result, function(x) {
+                                task_id <- setdiff(colnames(x),
+                                                   c("model_id", "output_type", "output_type_id", "value"))
+                                a <- x %>%
+                                        select(all_of(task_id)) %>%
+                                        summarise_all(n_distinct) %>%
+                                        unlist %>% unname()
+                                setequal(a, rep(1, length(task_id)))}
+                        )
+                ))
+
 })
