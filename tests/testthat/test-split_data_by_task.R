a7f686f47e2122d666ec21db85c0d3799b66bc11
mkim425 <minsu@umass.edu>
2024-01-30 11:27:53 -0500

    add a test to check if the data is split by unique combinations of task_ids

--- tests/testthat/test-split_data_by_task.R
+++ tests/testthat/test-split_data_by_task.R
@@ -3,11 +3,24 @@ library(hubUtils)
 
 test_that("split_data_by_task() groups data correctly", {
         hub_con <- connect_hub(system.file("testhubs/flusight", package = "hubUtils"))
-        forecast_data <- hub_con %>% dplyr::collect()
+        forecast_data <- hub_con %>%
+                dplyr::filter(output_type == "quantile") %>%
+                dplyr::collect() %>%
+                input_data_validation()
         result <- split_data_by_task(forecast_data)
 
         # Check if the result is a list
         expect_type(result, "list")
+
+        # Check if the list length matches the number of unique combinations of task_ids
+        expect_equal(
+                length(result),
+                forecast_data %>%
+                        select(all_of(task_id)) %>%
+                        reframe(across(everything(), as.character)) %>%
+                        summarise(n_distinct(do.call(paste, c(., sep = "")))) %>%
+                        as.numeric()
+                     )
 
         # Check if each data set has unique task_id
         expect_true(all(
